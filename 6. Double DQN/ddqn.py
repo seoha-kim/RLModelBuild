1803434508785afa0e83b845bdf2afc8589d0bb4
seoha-kim <chalcoalk@naver.com>
2020-09-21 20:47:54 +0900

    2020-09-21 update

---
+++ 6. Double DQN/ddqn.py
@@ -0,0 +1,95 @@
+import numpy as np
+import torch as T
+from dqn import DeepQNetwork
+from replay_buffer import ReplayBuffer
+
+class DDQNAgent(object):
+    def __init__(self, gamma, epsilon, lr, n_actions, input_dims, memory_size,
+                 batch_size, epsilon_min=0.01, epsilon_decay=5e-7, replace=1000,
+                 algorithm=None, env_name=None, check_point_dir = 'tmp/dqn'):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.lr = lr
+        self.n_actions = n_actions
+        self.input_dims = input_dims
+        self.batch_size = batch_size
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.replace_target_cnt = replace
+        self.algorithm = algorithm
+        self.env_name = env_name
+        self.check_point_dir = check_point_dir
+        self.action_space = [i for i in range(n_actions)]
+        self.learn_step_counter = 0
+
+        self.memory = ReplayBuffer(memory_size, input_dims, n_actions)
+        self.q_eval = DeepQNetwork(self.lr, self.n_actions, input_dims=self.input_dims,
+                                   name=self.env_name+'_'+self.algorithm+'_q_eval', check_point_dir=self.check_point_dir)
+        self.q_next = DeepQNetwork(self.lr, self.n_actions, input_dims=self.input_dims,
+                                   name=self.env_name+'_'+self.algorithm+'_q_next', check_point_dir=self.check_point_dir)
+
+    def store_transition(self, state, action, reward, new_state, done):
+        self.memory.store_transition(state, action, reward, new_state, done)
+
+    def sample_memory(self):
+        state, action, reward, new_state, done = self.memory.sample_buffer(self.batch_size)
+
+        states = T.tensor(state).to(self.q_eval.device)
+        rewards = T.tensor(reward).to(self.q_eval.device)
+        dones = T.tensor(done).to(self.q_eval.device)
+        actions = T.tensor(action).to(self.q_eval.device)
+        new_states = T.tensor(new_state).to(self.q_eval.device)
+        return states, actions, rewards, new_states, dones
+
+    def choose_action(self, observation):
+        if np.random.random() > self.epsilon:
+            state = T.tensor([observation], dtype=T.float).to(self.q_eval.device)
+            actions = self.q_eval.forward(state)
+            action = T.argmax(actions).item()
+
+        else:
+            action = np.random.choice(self.action_space)
+
+        return action
+
+    def replace_target_network(self):
+        if self.replace_target_cnt is not None and \
+            self.learn_step_counter % self.replace_target_cnt == 0:
+            self.q_next.load_state_dict(self.q_eval.state_dict())
+
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon - self.epsilon_decay if self.epsilon > self.epsilon_min else self.epsilon_min
+
+    def learn(self):
+        if self.memory.memory_cntr < self.batch_size:
+            return
+
+        self.q_eval.optimizer.zero_grad()
+        self.replace_target_network()
+        states, actions, rewards, new_states, dones = self.sample_memory()
+
+        indices = np.arange(self.batch_size)
+
+        q_pred = self.q_eval.forward(states)[indices, actions]
+        q_next = self.q_next.forward(new_states)
+        q_eval = self.q_eval.forward(new_states)
+
+        max_actions = T.argmax(q_eval, dim=1)
+        q_next[dones] = 0.0
+
+        q_target = rewards + self.gamma * q_next[indices, max_actions]
+        loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
+        loss.backward()
+
+        self.q_eval.optimizer.step()
+        self.learn_step_counter += 1
+        self.decrement_epsilon()
+
+    def save_models(self):
+        self.q_eval.save_checkpoint()
+        self.q_next.save_checkpoint()
+
+    def load_models(self):
+        self.q_eval.load_checkpoint()
+        self.q_next.load_checkpoint()
+
---
+++ 6. Double DQN/replay_buffer.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class ReplayBuffer(object):
+    def __init__(self, max_size, input_shape, n_actions):
+        self.memory_size = max_size
+        self.memory_cntr = 0
+        self.state_memory = np.zeros((self.memory_size, *input_shape), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.momory_size, *input_shape), dtype=np.float32)
+
+        self.action_memory = np.zeros(self.memory_size, dtype=np.int64)
+        self.reward_memory = np.zeros(self.memory_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.memory_size, dtype=np.bool)
+
+    def store_transition(self, state, action, reward, new_state, done):
+        index = self.memory_cntr % self.memory_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = new_state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+        self.memory_cntr += 1
+
+    def sample_buffer(self, batch_size):
+        max_memory = min(self.memory_cntr, self.memory_size)
+        batch = np.random.choice(max_memory, batch_size, replace=False)
+
+        states = self.state_memory[batch]
+        actions = self.state_memory[batch]
+        rewards = self.reward_memory[batch]
+        new_states = self.new_state_memory[batch]
+        terminal = self.terminal_memory[batch]
+        return states, actions, rewards, new_states, terminal
